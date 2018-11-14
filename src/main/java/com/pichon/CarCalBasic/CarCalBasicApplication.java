e344c8828676a66f07d659735116254ddfd550ce
nicoangelico <nicoangelico@gmail.com>
2018-11-14 11:08:22 -0300

    Add hello world

--- src/main/java/com/pichon/CarCalBasic/CarCalBasicApplication.java
+++ src/main/java/com/pichon/CarCalBasic/CarCalBasicApplication.java
@@ -2,11 +2,24 @@ package com.pichon.CarCalBasic;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
 
 @SpringBootApplication
+@RestController
 public class CarCalBasicApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CarCalBasicApplication.class, args);
 	}
+	
+	@RequestMapping(value="/marcas", method=RequestMethod.GET)
+    public String hello() {
+            return "Hello Spring Boot!";
+    }
+
+	
 }
