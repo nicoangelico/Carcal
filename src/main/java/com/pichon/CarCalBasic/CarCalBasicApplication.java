f813c3a2e65dce44b7babc907548a334411621b9
nicoangelico <nicoangelico@gmail.com>
2018-11-14 10:22:24 -0300

    Fist commit

---
+++ src/main/java/com/pichon/CarCalBasic/CarCalBasicApplication.java
@@ -0,0 +1,12 @@
+package com.pichon.CarCalBasic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarCalBasicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarCalBasicApplication.class, args);
+	}
+}
---
+++ src/test/java/com/pichon/CarCalBasic/CarCalBasicApplicationTests.java
@@ -0,0 +1,16 @@
+package com.pichon.CarCalBasic;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CarCalBasicApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
